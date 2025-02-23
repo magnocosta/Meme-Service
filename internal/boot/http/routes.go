2ac306a9fc59b83159ccdf6238fb633695a37980
Magno Costa <magnocosta.br@gmail.com>
2025-02-23 16:38:49 -0300

    Add list customer to the http handle

--- internal/boot/http/routes.go
+++ internal/boot/http/routes.go
@@ -1,7 +1,8 @@
 package http
 
 import (
-	addCustomer "meme_service/internal/app/addCustomer"
+	createCustomer "meme_service/internal/app/customer/create"
+	listCustomer "meme_service/internal/app/customer/list"
 	"meme_service/internal/db"
 	"net/http"
 	"database/sql"
@@ -14,8 +15,11 @@ func Init() {
   router := mux.NewRouter()
   conn := createDB()
 
-  createCustomerHandler := addCustomer.NewHTTPHandler(conn)
+  createCustomerHandler := createCustomer.NewHTTPHandler(conn)
   router.HandleFunc("/v1/customers", createCustomerHandler.Handle).Methods("Post")
+
+  listCustomerHandler := listCustomer.NewHTTPHandler(conn)
+  router.HandleFunc("/v1/customers", listCustomerHandler.Handle).Methods("Get")
 
   // router.HandleFunc("/v1/memes", getMemes.ListMemesHandle).Methods("Get")
   // router.HandleFunc("/v1/customers", listCustomers.ListCustomersHandle).Methods("Get")
